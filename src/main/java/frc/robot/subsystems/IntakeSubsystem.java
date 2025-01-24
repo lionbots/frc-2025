6cc13abe1d7a9a645d3de5a70e09c6d2f407bb81
q29ll <lin11239234@gmail.com>
2025-01-23 17:24:09 -0800

    created new intake branch and imported libraries

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,71 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.servohub.ServoHub.ResetMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.*;
+
+
+public class IntakeSubsystem extends SubsystemBase {
+  /** Create instance variables for the motors*/
+  private final SparkMax pivotMotor = new SparkMax(0, MotorType.kBrushless);
+  private final SparkMax intakeMotor = new SparkMax(1, MotorType.kBrushless);
+
+  public IntakeSubsystem() {
+    setMotorIdleModes();
+  }
+
+  public void setMotorIdleModes() {
+    SparkMaxConfig idleMode = new SparkMaxConfig();
+    idleMode.idleMode(IdleMode.kBrake);
+
+    pivotMotor.configure(idleMode, com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    intakeMotor.configure(idleMode, com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command intakeCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
