c56d7f13264c02066094668eb618fcdf90913997
ricemuncher925 <guaneywu20081023@gmail.com>
2025-01-25 12:44:27 -0800

    set all the motors to brake mode

--- src/main/java/frc/robot/subsystems/OuttakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/OuttakeSubsystem.java
@@ -1,15 +1,13 @@
 package frc.robot.subsystems;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public class OuttakeSubsystem extends SubsystemBase {
   //left motor on launcher
@@ -20,8 +18,17 @@ public class OuttakeSubsystem extends SubsystemBase {
   private final SparkMax pivotMotor = new SparkMax(0,MotorType.kBrushless);
 
     public OuttakeSubsystem () {
-
+      
     }
+
+    public void idleMotor() {
+      SparkMaxConfig idleMode = new SparkMaxConfig();
+      idleMode.idleMode(IdleMode.kBrake);
+      leftLauncherMotor.configure(idleMode, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+      rightLauncherMotor.configure(idleMode, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+      pivotMotor.configure(idleMode, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }
+
     public Command outtakeMethodCommand() {
         // Inline construction of command goes here.
         // Subsystem::RunOnce implicitly requires `this` subsystem.
