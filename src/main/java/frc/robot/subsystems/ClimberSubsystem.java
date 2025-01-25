b98fcd683d1f68ecee4feea160b565a7e50bd609
notCgu <moonwatcher584@gmail.com>
2025-01-25 12:31:57 -0800

    Instantiate instance variable in ClimberSubsystem

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -7,15 +7,16 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.*;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 public class ClimberSubsystem extends SubsystemBase {
 
-  private SparkMax climberMotor;
+  private final SparkMax climberMotor = new SparkMax(0, MotorType.kBrushless);
 
   /** Creates a new ClimberSubsystem. */
   public ClimberSubsystem() {
-
+    
   }
 
   /**
