35f9a8d49c080e1189ea08510be6359de6b39227
ricemuncher925 <>
2025-02-25 09:01:18 -0800

    in the pivot and outtake motor set the speed to 0 when command ends

--- src/main/java/frc/robot/commands/OuttakeCommand.java
+++ src/main/java/frc/robot/commands/OuttakeCommand.java
@@ -31,7 +31,9 @@ public class OuttakeCommand extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    outtake.setOuttakeSpeed(0);
+  }
 
   // Returns true when the command should end.
   @Override
