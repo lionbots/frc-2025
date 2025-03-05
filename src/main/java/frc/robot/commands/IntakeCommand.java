471e771549f3c8cc017953e8d8d1fa57e8364ec3
GenesisYLopezG <yaretziglopez04@gmail.com>
2025-03-04 16:27:56 -0800

    Implement launcher into intake command

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -5,17 +5,20 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.OuttakeSubsystem;
 import edu.wpi.first.wpilibj2.command.*;
 import java.util.function.Supplier;
 
 // An Intake command that uses an Intake subsystem.
 public class IntakeCommand extends Command {
     private final IntakeSubsystem intake;
+    private final OuttakeSubsystem launcher;
     private final Supplier<Double> intakeSpeed;
 
-  public IntakeCommand(IntakeSubsystem intake, Supplier<Double> intakeSpeed) {
+  public IntakeCommand(IntakeSubsystem intake, OuttakeSubsystem launcher, Supplier<Double> intakeSpeed) {
     this.intake = intake;
     this.intakeSpeed = intakeSpeed;
+    this.launcher = launcher;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(intake);
   }
@@ -28,6 +31,8 @@ public class IntakeCommand extends Command {
   @Override
   public void execute() {
     intake.setIntakeSpeed(intakeSpeed.get());
+    launcher.setOuterSpeed(intakeSpeed.get());
+    launcher.setInnerSpeed(intakeSpeed.get());
   }
 
   // Called once the command ends or is interrupted.
