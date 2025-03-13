c00e8fabd2fc44d9fe37b2fdf30e18277760b49f
q29ll <lin11239234@gmail.com>
2025-03-13 16:53:05 -0700

    Create new controller for operater

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,7 +14,8 @@ package frc.robot;
  */
 public final class Constants {
   public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
+    public static final int driverControllerPort = 0;
+    public static final int operatorControllerPort = 5;
   }
   public static class DriveConstants {
     public static final int frDeviceId = 1;
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -22,14 +22,13 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final OuttakeSubsystem outtake = new OuttakeSubsystem();
-  private final OuttakeSubsystem outtakePivot = new OuttakeSubsystem();
   private final IntakeSubsystem intake = new IntakeSubsystem();
-  private final IntakeSubsystem intakePivot = new IntakeSubsystem();
   private final DrivebaseSubsystem drivebase = new DrivebaseSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController driverController = new CommandXboxController(OperatorConstants.driverControllerPort);
+  private final CommandXboxController operater = new CommandXboxController(OperatorConstants.operatorControllerPort);
+
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
