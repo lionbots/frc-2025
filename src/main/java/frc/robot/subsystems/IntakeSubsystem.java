60f9ec4a0f4614e0155aa718cbcc086f31366de8
Matthew Lin <linmatthew2007@gmail.com>
2025-03-13 20:11:53 -0700

    Remove extra motor and unnecessary methods and imports in intake subsystem

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -21,8 +21,7 @@ import com.revrobotics.spark.*;
 public class IntakeSubsystem extends SubsystemBase {
   //Create instance variables for the motors
   private final SparkMax pivotMotor = new SparkMax(0, MotorType.kBrushless);
-  private final SparkMax intakeRMotor = new SparkMax(1, MotorType.kBrushless);
-  private final SparkMax intakeLMotor = new SparkMax(2, null);
+  private final SparkMax intakeMotor = new SparkMax(1, MotorType.kBrushless);
   //Create instance variables for the encoders
   private final RelativeEncoder pivotEncoder = pivotMotor.getEncoder();
 
@@ -37,14 +36,13 @@ public class IntakeSubsystem extends SubsystemBase {
     idleMode.idleMode(IdleMode.kBrake);
 
     pivotMotor.configure(idleMode, com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    intakeRMotor.configure(idleMode, com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    intakeLMotor.configure(idleMode, com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    intakeMotor.configure(idleMode, com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
   //Method for setting intake speed
   public void setIntakeSpeed(double intakeSpeed) {
-    intakeRMotor.set(intakeSpeed);
-    intakeLMotor.set(intakeSpeed);
+    intakeMotor.set(intakeSpeed);
+    intakeMotor.set(intakeSpeed);
   }
 
   //Method for setting pivot speed
@@ -55,39 +53,5 @@ public class IntakeSubsystem extends SubsystemBase {
   //Method to get position of pivot
   public double getPivotPosition() {
     return pivotEncoder.getPosition();
-  }
-
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public Command intakeCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
-  }
-
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
   }
 }
