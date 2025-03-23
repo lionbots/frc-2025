2a588aa75949b6a4353f7425504fa9d6c152fab9
Matthew Lin <linmatthew2007@gmail.com>
2025-03-23 13:06:50 -0700

    Stop intake motors after intaking

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -38,7 +38,9 @@ public class IntakeCommand extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    intake.setIntakeSpeed(0);
+  }
 
   // Returns true when the command should end.
   @Override
