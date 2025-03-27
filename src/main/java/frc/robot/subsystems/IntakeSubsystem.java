aff3f35ac7739036be3ff9347f80231a7c44566a
Nathan Ko <konathan005@gmail.com>
2025-03-26 17:10:03 -0700

    Iterate simulated intake flywheel

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -5,19 +5,18 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.simulation.BatterySim;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 import edu.wpi.first.wpilibj.simulation.RoboRioSim;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.ClimberConstants;
 import frc.robot.Constants.IntakeConstants;
 
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.sim.SparkMaxSim;
-import com.revrobotics.sim.SparkRelativeEncoderSim;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
@@ -36,8 +35,8 @@ public class IntakeSubsystem extends SubsystemBase {
   //Create instance variable for the motor simulation
   private final SparkMaxSim pivotMotorSim = new SparkMaxSim(pivotMotor, DCMotor.getNEO(1));
   private final SparkMaxSim intakeMotorSim = new SparkMaxSim(intakeMotor, DCMotor.getNEO(1));
-  private final SingleJointedArmSim armSim = new SingleJointedArmSim(DCMotor.getNEO(1), 1, 1, 0.2794, 0, Math.PI, false, 0);
-  private final SparkRelativeEncoderSim pivotEncoderSim = pivotMotorSim.getRelativeEncoderSim();
+  private final SingleJointedArmSim pivotSim = new SingleJointedArmSim(DCMotor.getNEO(1), 1, 1, 0.2794, 0, Math.PI, false, 0);
+  private final FlywheelSim intakeFlywheelSim = new FlywheelSim(LinearSystemId.createFlywheelSystem(DCMotor.getNeo550(1), 1, 4), DCMotor.getNeo550(1));
 
   // Constructor to access the brake mode method
   public IntakeSubsystem() {
@@ -46,20 +45,17 @@ public class IntakeSubsystem extends SubsystemBase {
 
   @Override
   public void simulationPeriodic() {
-    
-    this.armSim.setInput(pivotMotorSim.getAppliedOutput() * RoboRioSim.getVInVoltage());
-    this.armSim.update(0.02);
+    double vInVoltage = RoboRioSim.getVInVoltage();
 
-    // simulated motor has velocity without getAppliedOutput()
-    if (pivotMotorSim.getAppliedOutput() == 0) {
-      this.armSim.setState(this.armSim.getAngleRads(), 0);
-      this.pivotMotorSim.iterate(0, RoboRioSim.getVInVoltage(), 0.02);
-    } else {
-      this.pivotMotorSim.iterate(Units.radiansPerSecondToRotationsPerMinute(this.armSim.getVelocityRadPerSec()), RoboRioSim.getVInVoltage(), 0.02);
-    }
-    RoboRioSim.setVInVoltage(BatterySim.calculateDefaultBatteryLoadedVoltage(this.armSim.getCurrentDrawAmps()));
+    this.pivotSim.setInput(pivotMotorSim.getAppliedOutput() * vInVoltage);
+    this.pivotSim.update(0.02);
+    this.pivotMotorSim.iterate(Units.radiansPerSecondToRotationsPerMinute(this.pivotSim.getVelocityRadPerSec()), vInVoltage, 0.02);
 
+    this.intakeFlywheelSim.setInput(intakeMotorSim.getAppliedOutput() * vInVoltage);
+    this.intakeFlywheelSim.update(0.02);
+    this.intakeMotorSim.iterate(this.intakeFlywheelSim.getAngularVelocityRPM(), vInVoltage, 0.02);
 
+    RoboRioSim.setVInVoltage(BatterySim.calculateDefaultBatteryLoadedVoltage(this.pivotSim.getCurrentDrawAmps()));
   }
 
   public double getEncoder() {
@@ -77,6 +73,7 @@ public class IntakeSubsystem extends SubsystemBase {
 
   //Method for setting intake speed
   public void setIntakeSpeed(double intakeSpeed) {
+    // System.out.println(intakeSpeed);
     intakeMotor.set(intakeSpeed);
   }
 
@@ -89,19 +86,6 @@ public class IntakeSubsystem extends SubsystemBase {
   public double getPivotPosition() {
     return pivotEncoder.getPosition();
   }
-
-  /*
-   * Get climber position
-   * @return the position of the climber motor according to the encoder
-   */
-
-  public double getPosition(){
-    return RobotBase.isSimulation() ? pivotEncoderSim.getPosition() : pivotEncoderSim.getPosition();
-  }
-  /*
-   * Sets the speed of the motor. 
-   * @param the desired speed for the motor
-   */
 
   public void periodic() {
     SmartDashboard.putNumber("Encoder", getEncoder());
